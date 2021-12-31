5f45aeb50aa9f39f44ea7b8481926bef8f72c637
v1k7oriy <75143897+v1k7oriy@users.noreply.github.com>
2021-12-31 17:41:38 +0300

    Initial commit

---
+++ 1st.py
@@ -0,0 +1,72 @@
+import telebot
+import random
+from telebot import types
+
+bot = telebot.TeleBot('5057369197:AAGAE9mEo4EPBZhcezOM7ojBKRQ-0AXtaq4')
+
+first = ["Пришло время заняться собой.",
+         "Посвятите день только себя!",
+         "Возможна очень приятная новость.",
+         "День наполнения энергии!.",
+         "У вас грандиозные планы!",
+         "Сегодня вы просто излучаете уверенность в себе."]
+second = ["Порадуйте себя, ведь вы этого достойны", "Звезды судят исполнение желаний",
+          "Обратите внимание на свое здоровье",
+          "Есть шанс закрыть сложные вопросы, которые не давали вам спокойно жить последние месяцы.",
+          "Вам придется взять себя в руки, чтобы справиться с проблемами."]
+third = ["Также в вашей жизни может появиться таинственный незнакомец...",
+         "Приготовьтесь получать подарки не только от любимого человека, но и от судьбы.",
+         "Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.",
+         "Вы осень быстро соберетесь и решите все вопросы.",
+         "В этот день звезды рекомендуют вам обдумать предстоящие действия, связанные с финансами."]
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    # Если написали «Привет»
+    if message.text == "Привет":
+        # Пишем приветствие
+        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
+        # Готовим кнопки
+        keyboard = types.InlineKeyboardMarkup()
+        # По очереди готовим текст и обработчик для каждого знака зодиака
+        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
+        # И добавляем кнопку на экран
+        keyboard.add(key_oven)
+        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+        keyboard.add(key_telec)
+        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
+        keyboard.add(key_bliznecy)
+        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
+        keyboard.add(key_rak)
+        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
+        keyboard.add(key_lev)
+        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+        keyboard.add(key_deva)
+        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+        keyboard.add(key_vesy)
+        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+        keyboard.add(key_scorpion)
+        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
+        keyboard.add(key_strelec)
+        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
+        keyboard.add(key_kozerog)
+        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
+        keyboard.add(key_vodoley)
+        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
+        keyboard.add(key_ryby)
+        # Показываем все кнопки сразу и пишем сообщение о выборе
+        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+# Обработчик нажатий на кнопки
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    # Если нажали на одну из 12 кнопок — выводим гороскоп
+    if call.data == "zodiac":
+        # Формируем гороскоп
+        msg = random.choice(first) + ' ' + random.choice(second) + ' ' + ' ' + random.choice(third)
+        # Отправляем текст в Телеграм
+        bot.send_message(call.message.chat.id, msg)
+# Запускаем постоянный опрос бота в Телеграме
+bot.polling(none_stop=True, interval=0)
